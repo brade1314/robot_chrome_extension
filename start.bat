e05f7153dd0a1c1925333fa297eb07f6ded1cd7d
brade1314@gmail.com <brade1314@gmail.com>
2019-03-29 15:25:17 +0800

    修改.bat启动命令

--- start.bat
+++ start.bat
@@ -11,6 +11,9 @@ echo.
 echo "| ->> 请确认电脑已安装chrome浏览器，并浏览器已关闭"
 echo.
 echo "| ->> 当前目录是:" %~dp0%
+echo.
+echo "| ->> 如果浏览器已经打开，会关闭后重新打开，并加载插件；未打开则打开浏览器同时加载插件"
+for /f "tokens=2 skip=3 delims= " %%i in ('tasklist /fi "imagename eq chrome.exe" /fi "STATUS eq running" ') do (TASKKILL /f /pid %%i)
 
 echo.
 start chrome --load-extension=%~dp0% --no-first-run
